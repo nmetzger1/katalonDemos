d58fcf89aa568fdef1b05f9f02843d8e044e012c
Nicholas Metzger <nmetzger@flvs.net>
2023-02-28 08:53:29 -0500

    loops and data driven stuffs

---
+++ LoginExample/Test Suites/Data Driven/Google.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Google</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ced07148-50b7-4ecb-b319-a31765e735a8</testSuiteGuid>
+   <testCaseLink>
+      <guid>63eb5953-20ff-487c-8255-de967e16391c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <iterationNameVariable>
+         <defaultValue>''</defaultValue>
+         <description></description>
+         <id>d1c6af60-7b3a-4f43-99b5-cb0b7a8b477f</id>
+         <masked>false</masked>
+         <name>searchTerm</name>
+      </iterationNameVariable>
+      <testCaseId>Test Cases/DataDriven/Google Search - DD</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>13db4002-ce2b-4cea-9312-6142491eb7bc</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/searchTerms</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>13db4002-ce2b-4cea-9312-6142491eb7bc</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Search Terms</value>
+         <variableId>d1c6af60-7b3a-4f43-99b5-cb0b7a8b477f</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
